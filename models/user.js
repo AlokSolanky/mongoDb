6e4d49b27d05c3b56e5c1d360cb34a35ef683d1e
AlokSolanky <aloksolanky1@gmail.com>
2023-10-08 12:10:56 +0530

    delete cart done

--- models/user.js
+++ models/user.js
@@ -46,6 +46,16 @@ userSchema.methods.addToCart = function (product) {
   this.cart = updatedCart;
   return this.save();
 };
+
+userSchema.methods.deleteCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
+
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
 module.exports = mongoose.model("User", userSchema);
 // const mongodb = require("mongodb");
 // const { get } = require("../routes/admin");
